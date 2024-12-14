724de5814a6200f70bf3a3c111a2a0f27343d920
elpatrinum <meb-bak@outlook.fr>
2024-12-15 00:20:29 +0100

    autosave

--- Snort_Alert_Response.cpp
+++ Snort_Alert_Response.cpp
@@ -99,7 +99,7 @@ int main()
                         std::string ip = extractIPAddress(match.str(0));
                         std::cout << "\033[1;32mBlocking IP: \033[1;37m" << ip << "\033[1;32m due to alert: \033[1;37m" << alert_keyword << "\033[0m" << std::endl;
                         std::string command = "iptables -A INPUT -s " + ip + " -j DROP";
-                        std::system(command.c_str());
+                        // std::system(command.c_str());
                         l_file << getCurrentDateTime() << " Blocking IP: " << ip << " due to alert: " << alert_keyword << std::endl;
                     }
                     break;
@@ -109,7 +109,8 @@ int main()
         else if (file.eof())
         {
             file.clear();
-            usleep(50);
+			file.seekg(0, std::ios::cur);
+            usleep(500);
         }
         else
         {
